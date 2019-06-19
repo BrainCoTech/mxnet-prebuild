f015febe40479fddfac58a4822ba6e29e23201c6
zlargon <zlargon1988@gmail.com>
2019-06-19 08:53:01 -0400

    [release] release android and ios with archs

--- release.sh
+++ release.sh
@@ -11,9 +11,8 @@ MXNET_VERSION=$(git rev-parse --short HEAD)
 
 cd $root
 echo $MXNET_VERSION > release/MXNET_VERSION
-cp mxnet/include/mxnet/c_predict_api.h          release
-cp mac/shared/libmxnet_predict.dylib            release/libmxnet_predict-mac.dylib
-cp mac/static/libmxnet_predict.a                release/libmxnet_predict-mac.a
-cp ios/dist/iphoneos/libmxnet_predict.a         release/libmxnet_predict-ios-iphone.a
-cp ios/dist/iphonesimulator/libmxnet_predict.a  release/libmxnet_predict-ios-simulator.a
-cp ios/dist/universal/libmxnet_predict.a        release/libmxnet_predict-ios-universal.a
+cp mxnet/include/mxnet/c_predict_api.h      release
+cp mac/shared/libmxnet_predict.dylib        release/libmxnet_predict-mac.dylib
+cp mac/static/libmxnet_predict.a            release/libmxnet_predict-mac.a
+cp ios/dist/*                               release/
+cp android/dist/*                           release/
