6d20b5018455f426b1624ad9f5976d17ef30dbdc
zlargon <zlargon1988@gmail.com>
2019-07-01 16:29:47 -0400

    [release] add x86_64 to mac releases

--- release.sh
+++ release.sh
@@ -11,8 +11,8 @@ MXNET_VERSION=$(git rev-parse --short HEAD)
 
 cd $root
 echo $MXNET_VERSION > release/MXNET_VERSION
-cp mxnet/include/mxnet/c_predict_api.h      release
-cp mac/shared/libmxnet_predict.dylib        release/libmxnet_predict-mac.dylib
-cp mac/static/libmxnet_predict.a            release/libmxnet_predict-mac.a
-cp ios/dist/*                               release/
-cp android/dist/*                           release/
+cp mxnet/include/mxnet/c_predict_api.h   release
+cp mac/shared/libmxnet_predict.dylib     release/libmxnet_predict-mac-x86_64.dylib
+cp mac/static/libmxnet_predict.a         release/libmxnet_predict-mac-x86_64.a
+cp ios/dist/*                            release/
+cp android/dist/*                        release/
