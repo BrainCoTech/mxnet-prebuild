fcff06afeecf01925e333e6e9678b6488bde9771
zlargon <zlargon1988@gmail.com>
2019-07-08 14:59:14 -0400

    [android] build libmxnet_predict.a by script and remove LDFLAGS

--- android/build-android-mxnet.sh
+++ android/build-android-mxnet.sh
@@ -5,67 +5,145 @@ root=$(pwd)
 # Android SDK API Level (API 21 for Android 5.0)
 API="21"
 
+# color code
+RED="\033[0;31m"
+YELLOW="\033[1;33m"
+RESET="\033[0m"
+
+# echo yellow
+function echo_y () {
+    echo -e "${YELLOW}$@${RESET}"
+}
+
+# echo red
+function echo_r () {
+    echo -e "${RED}$@${RESET}"
+}
+
 # clean
 rm -rf dist
 mkdir -p dist
 
-# check NDK
-if [ -z "$ANDROID_NDK_ROOT" ]; then
-    echo "[android] environment variable 'ANDROID_NDK_ROOT' need to be setup"
+# ENV: check Python 2.x
+PY_VERSION=$(python -V 2>&1)
+if [ -z "$(echo ${PY_VERSION} | grep 'Python 2.')" ]; then
+    echo_r "[android] required Python 2.x, but your python is (${PY_VERSION})"
     exit 1
 fi
 
-# check toolchain platform and export to PATH
+# ENV: check NDK
+if [ -z "$ANDROID_NDK_ROOT" ]; then
+    echo_r "[android] environment variable 'ANDROID_NDK_ROOT' need to be setup"
+    exit 1
+fi
+
+# ENV: export toolchain platform to PATH
 cd ${ANDROID_NDK_ROOT}/toolchains/llvm/prebuilt
 PLATFORM=$(ls -1 | head -1)     # e.g. darwin-x86_64
 if [ -z "$PLATFORM" ]; then
-    echo "[android] get toolchain platform failed"
+    echo_r "[android] get toolchain platform failed"
     exit 1
 fi
 export PATH="$PATH:${ANDROID_NDK_ROOT}/toolchains/llvm/prebuilt/${PLATFORM}/bin"
 
-# Makefile: remove hard-float flag
-cd ${root}/../mxnet/amalgamation/
-git checkout -- .
-cat Makefile \
-    | sed 's/-mhard-float -D_NDK_MATH_NO_SOFTFP=1//' \
-    | sed 's/ -lm_hard//' \
-    > tmp.mk
-mv tmp.mk Makefile
+# mxnet and blas root
+MXNET_ROOT=${root}/../mxnet
+BLAS_ROOT=${root}/openblas
 
-# build function
+# function: build
 function build () {
-    echo "ARCH = ${ARCH}"
-    echo "AR   = ${AR}"
-    echo "CC   = ${CC}"
-    echo "CXX  = ${CXX}"
+    cd ${MXNET_ROOT}/amalgamation
 
-    # build mxnet object file
-    OPTIONS="ANDROID=1 OPENBLAS_ROOT=${root}/openblas/${ARCH}"
-    cd ${root}/../mxnet/amalgamation/
-    make clean
-    make mxnet_predict-all.cc ${OPTIONS}
+    echo "[${ARCH}] AR = ${AR}"
+    echo "[${ARCH}] CC = ${CC}"
+    echo "[${ARCH}] CXX = ${CXX}"
+
+    # CFLAGS
+    CFLAGS="-std=c++11 -Wno-unknown-pragmas -Wall"
+    CFLAGS+=" -DMSHADOW_USE_CBLAS=1"
+    CFLAGS+=" -DMSHADOW_USE_SSE=0"
+    CFLAGS+=" -DDISABLE_OPENMP=1"
+    CFLAGS+=" -DMSHADOW_USE_CUDA=0"
+    CFLAGS+=" -DMSHADOW_USE_MKL=0"
+    CFLAGS+=" -DMSHADOW_RABIT_PS=0"
+    CFLAGS+=" -DMSHADOW_DIST_PS=0"
+    CFLAGS+=" -DDMLC_LOG_STACK_TRACE=0"
+    CFLAGS+=" -DMSHADOW_FORCE_STREAM"
+    CFLAGS+=" -DMXNET_USE_OPENCV=0"
+    CFLAGS+=" -DMXNET_PREDICT_ONLY=1"
+    CFLAGS+=" -DMSHADOW_USE_F16C=0"
+    CFLAGS+=" -I${BLAS_ROOT}/${ARCH}"
+    CFLAGS+=" -I${BLAS_ROOT}/${ARCH}/include"
+
+    # clean
+    echo_y "[${ARCH}] clean: remove nnvm.d dmlc.d mxnet_predict0.d mxnet_predict-all.cc mxnet_predict-all.o"
+    rm -rf nnvm.d               \
+        dmlc.d                  \
+        mxnet_predict0.d        \
+        mxnet_predict-all.cc    \
+        mxnet_predict-all.o
+
+    # nnvm.d
+    echo_y "[${ARCH}] build nnvm.d"
+    ./prep_nnvm.sh
+
+    # dmlc.d
+    echo_y "[${ARCH}] build dmlc.d"
+    ${CXX} ${CFLAGS} -M -MT dmlc-minimum0.o         \
+        -I${MXNET_ROOT}/3rdparty/dmlc-core/include  \
+        -D__MIN__=0 dmlc-minimum0.cc > dmlc.d
+
+    # mxnet_predict0.d
+    echo_y "[${ARCH}] build mxnet_predict0.d"
+    ${CXX} ${CFLAGS} -M -MT mxnet_predict0.o        \
+        -I${MXNET_ROOT}                             \
+        -I${MXNET_ROOT}/include                     \
+        -I${MXNET_ROOT}/3rdparty/mshadow            \
+        -I${MXNET_ROOT}/3rdparty/dmlc-core/include  \
+        -I${MXNET_ROOT}/3rdparty/dmlc-core/src      \
+        -I${MXNET_ROOT}/3rdparty/tvm/nnvm/include   \
+        -I${MXNET_ROOT}/3rdparty/dlpack/include     \
+        -D__MIN__=0 mxnet_predict0.cc > mxnet_predict0.d
+
+    # add dmlc.d and nnvm.d to mxnet_predict0.d
+    echo_y "[${ARCH}] add dmlc.d and nnvm.d to mxnet_predict0.d"
+    cat dmlc.d >> mxnet_predict0.d
+    cat nnvm.d >> mxnet_predict0.d
+
+    # mxnet_predict-all.cc
+    echo_y "[${ARCH}] build mxnet_predict-all.cc"
+    python ./amalgamation.py    \
+        mxnet_predict0.d        \
+        dmlc-minimum0.cc        \
+        nnvm.cc                 \
+        mxnet_predict0.cc       \
+        mxnet_predict-all.cc    \
+        0 1                         # MIN=0 ANDROID=1
 
     # modify mxnet_predict-all.cc
+    echo_y "[${ARCH}] modify mxnet_predict-all.cc"
     cat mxnet_predict-all.cc \
         | sed 's/#include <TargetConditionals.h>//' \
         | sed 's/#include <x86intrin.h>/#include <endian.h>/' \
         > tmp.cc
     mv tmp.cc mxnet_predict-all.cc
 
-    # build mxnet_predict-all.o
-    make mxnet_predict-all.o ${OPTIONS}
+    # mxnet_predict-all.o
+    echo_y "[${ARCH}] build mxnet_predict-all.o"
+    ${CXX} ${CFLAGS} -fPIC \
+        -c mxnet_predict-all.cc \
+        -o ${root}/dist/mxnet_predict-all.o           # destination: ${root}/dist
 
-    # move mxnet_predict-all.o to dist
-    mv mxnet_predict-all.o ${root}/dist
-
-    # merge openblas into libmxnet_predict.a
+    # merge openblas.a into libmxnet_predict.a
+    echo_y "[${ARCH}] merge openblas.a into libmxnet_predict.a"
     cd ${root}/dist
     ${AR} x ${root}/openblas/${ARCH}/lib/*.dev.a      # generate openblas object files
     ${AR} rcs libmxnet_predict-android-${ARCH}.a *.o  # combine all object files to a static library
 
     # remove object files
     rm -rf *.o
+
+    echo_y "[${ARCH}] libmxnet_predict-android-${ARCH}.a"
 }
 
 # 1. armeabi-v7a
@@ -95,7 +173,3 @@ export AR="x86_64-linux-android-ar"
 export CC="x86_64-linux-android${API}-clang"
 export CXX="x86_64-linux-android${API}-clang++"
 build
-
-# Makefile: reset changed files in MXNet
-cd ${root}/../mxnet/amalgamation/
-git checkout -- .
